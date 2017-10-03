fff490c0d0c5c51806c22e19c6af0f08f5bc44ea
Chuck Underwood <chillieguy@live.com>
2017-10-02 20:17:26 -0700

    Fixed errors according to homework requirements

--- main.py
+++ main.py
@@ -1,74 +1,75 @@
+import os
+import re
 import webapp2
 import jinja2
-import os
-import time
 
 from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
-jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
-                               autoescape=True)
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
-class Post(db.Model):
-    title = db.StringProperty(required=True)
-    body = db.TextProperty(required=True)
-    author = db.StringProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
 class Handler(webapp2.RequestHandler):
-    """Helper function for writing and rendering output
-       Code taken from Udacity and Steve Huffman"""
     def write(self, *a, **kw):
         self.response.out.write(*a, **kw)
 
     def render_str(self, template, **params):
-        t = jinja_env.get_template(template)
-        return t.render(params)
+        return render_str(template, **params)
 
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
-class MainPage(Handler):
-    """Main entry point into app"""
-    def render_front_page(self, title="", body="", author="", error=""):
-        posts = db.GqlQuery("SELECT * FROM Post ORDER BY created DESC")
+class Post(db.Model):
+    title = db.StringProperty(required = True)
+    body = db.TextProperty(required = True)
+    author = db.StringProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
 
-        self.render("front_page.html", title=title, body=body, author=author, error=error, posts=posts)
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("post.html", p = self)
 
+class Front(Handler):
     def get(self):
-        self.render_front_page()
+        posts = db.GqlQuery("select * from Post order by created desc limit 10")
+        self.render('front_page.html', posts = posts)
+
+class PostPage(Handler):
+    def get(self, post_id):
+        key = db.Key.from_path('Post', int(post_id))
+        post = db.get(key)
+
+        if not post:
+            self.error(404)
+            return
+
+        self.render("post.html", post = post)
 
 class NewPost(Handler):
-    def render_newpost(self, title="", body="", author="Chuck Underwood", error=""):
-        self.render("newpost.html", title=title, body=body, author=author, error=error)
-
     def get(self):
-        self.render_newpost()
+        self.render("newpost.html")
 
     def post(self):
-        title = self.request.get("title")
-        body = self.request.get("body")
-        author = self.request.get("author")
+        title = self.request.get('title')
+        body = self.request.get('body')
+        author = self.request.get('author')
+
 
         if title and body and author:
             p = Post(title=title, body=body, author=author)
             p.put()
-
-            time.sleep(0.1)
-            self.redirect('/')
+            self.redirect('/%s' % str(p.key().id()))
         else:
-            error = "We need a title, body and author"
-            self.render_newpost(title, body, author, error)
+            error = "subject and content, please!"
+            self.render("newpost.html", title=title, body=body, author=author, error=error)
 
-class Delete(Handler):
-    def get(self):
-        db.delete(Post.all(keys_only=True))
-
-        time.sleep(0.1)
-        self.redirect('/')
-
-app = webapp2.WSGIApplication([
-    ('/', MainPage),
-    ('/newpost/', NewPost),
-    ('/delete', Delete)
-], debug=True)
+app = webapp2.WSGIApplication([('/', Front),
+                               ('/([0-9]+)', PostPage),
+                               ('/newpost', NewPost),
+                               ],
+                              debug=True)
